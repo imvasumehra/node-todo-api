58eb4af565de492f1ac19f62481d1db18b85c6dc
Vasu <vasumail007@gmail.com>
2018-07-11 14:38:46 +0530

    URL correction

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://vasumehra:vasu123@ds131711.mlab.com:31711/node-todo-api'|| 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
